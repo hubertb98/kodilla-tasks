cd3394760ea4090c469597c3e584dc31e1acdb5d
hubertb98 <hubertb.98.hb@gmail.com>
2020-09-05 11:32:55 +0200

    Test Error

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -1,16 +1,62 @@
 package com.crud.tasks.trello.client;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.crud.tasks.config.TrelloConfig;
+import com.crud.tasks.domain.TrelloBoardDto;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
-class TrelloClientTest {
-    @Autowired
+@RunWith(SpringRunner.class)
+public class TrelloClientTest {
+
+    @InjectMocks
     private TrelloClient trelloClient;
 
+    @MockBean
+    private RestTemplate restTemplate;
+
+    @MockBean
+    private TrelloConfig trelloConfig;
+
+
+    @Before
+    public void init() {
+        Mockito.when(trelloConfig.getTrelloApiEndpoint()).thenReturn("http://test.com");
+        Mockito.when(trelloConfig.getTrelloAppKey()).thenReturn("test");
+        Mockito.when(trelloConfig.getTrelloToken()).thenReturn("test");
+
+    }
     @Test
-    public void test() {
-        System.out.println(trelloClient.getTrelloBoards());
+    public void shouldFetchTrelloBoards() throws URISyntaxException {
+        // Given
+
+        TrelloBoardDto[] trelloBoards = new TrelloBoardDto[1];
+        trelloBoards[0] = new TrelloBoardDto("test_id", "test_board", new ArrayList<>());
+
+        URI uri = new URI("http://test.com/members/hubertburdy/boards?key=test&token=test&fields=name,id&lists=all");
+
+        Mockito.when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(trelloBoards);
+
+        // When
+        List<TrelloBoardDto> fetchedTrelloBoards = trelloClient.getTrelloBoards();
+
+        // Then
+        Assert.assertEquals(1, fetchedTrelloBoards.size());
+        Assert.assertEquals("test_id", fetchedTrelloBoards.get(0).getId());
+        Assert.assertEquals("test_board", fetchedTrelloBoards.get(0).getName());
+        Assert.assertEquals(new ArrayList<>(), fetchedTrelloBoards.get(0).getLists());
     }
 }
