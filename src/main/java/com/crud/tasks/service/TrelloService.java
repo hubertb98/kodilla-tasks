a38af2f9e40ee95438f4df082afea918fcbd4c8b
hubertb98 <hubertb.98.hb@gmail.com>
2020-11-25 17:33:43 +0100

    32.2 Creating a template and sending mail

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -2,6 +2,7 @@ package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.domain.CreatedTrelloCardDto;
+import com.crud.tasks.domain.Mail;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static java.util.Optional.ofNullable;
+
 @Service
 public class TrelloService {
     private static final String SUBJECT = "Tasks: New Trello card";
@@ -17,8 +20,8 @@ public class TrelloService {
     @Autowired
     private TrelloClient trelloClient;
 
-//    @Autowired
-//    private SimpleEmailService emailService;
+    @Autowired
+    private SimpleEmailService emailService;
 
     @Autowired
     private AdminConfig adminConfig;
@@ -29,9 +32,9 @@ public class TrelloService {
 
     public CreatedTrelloCardDto createdTrelloCard(final TrelloCardDto trelloCardDto) {
         CreatedTrelloCardDto newCard = trelloClient.createNewCard(trelloCardDto);
-//        ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(
-//                adminConfig.getAdminMail(), SUBJECT,
-//                "New card: " + card.getName() + "has been created on your Trello account")));
+        ofNullable(newCard).ifPresent(card -> emailService.send(new Mail(
+                adminConfig.getAdminMail(), SUBJECT,
+                "New card: " + card.getName() + "has been created on your Trello account")));
         return newCard;
     }
 }
